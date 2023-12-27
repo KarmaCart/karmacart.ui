fbfe3a6710fad337c651830022f13bec3719fa3f
andersbuck <buckles.anderson@gmail.com>
2023-12-26 23:20:38 -0500

    Change layout to hamburger style menu

--- src/App.js
+++ src/App.js
@@ -1,24 +1,35 @@
 import React, { useState } from 'react';
 import {
-  MenuFoldOutlined,
-  MenuUnfoldOutlined,
   InfoCircleOutlined,
   UserOutlined,
   ScanOutlined,
+  MenuOutlined
 } from '@ant-design/icons';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import { Layout, Menu, Button, theme, } from 'antd';
+import { Layout, Menu, Button, Drawer } from 'antd';
 import BarcodeScanner from './pages/BarcodeScanner';
 import LoginScreen from './pages/LoginScreen';
 import MoreInfo from './pages/MoreInfo'
-const { Header, Sider } = Layout;
+const { Header, Footer } = Layout;
+
 const App = () => {
 
-  const [collapsed, setCollapsed] = useState(false);
+  const [visible, setVisible] = useState(false);
 
-  const {
-    token: { colorBgContainer, colorBgMask },
-  } = theme.useToken();
+  function getItem(label, key, icon, children) {
+    return {
+      key,
+      icon,
+      children,
+      label,
+    };
+  }
+  
+  const items = [
+    getItem('Account', 'LoginScreen', <UserOutlined />),
+    getItem('Scan', 'BarcodeScanner', <ScanOutlined />),
+    getItem('More Info', 'MoreInfo', <InfoCircleOutlined />),
+  ];
 
   const handleMenuClick = (e) => {
     console.log('Menu item clicked:', e.key);
@@ -31,57 +42,42 @@ const App = () => {
     }
   };
 
+  const showDrawer = () => {
+    setVisible(true);
+  };
+
+  const onClose = () => {
+    setVisible(false);
+  };
+
   return (
-    <Layout>
-      <Sider trigger={null} collapsible collapsed={collapsed}>
-        <div style={{
-            height: 32,
-            margin: 16,
-            background: colorBgMask,
-            borderRadius: 6,
-        }} />
-        <Menu
-          theme="dark"
-          mode="inline"
-          defaultSelectedKeys={['1']}
-          onClick={handleMenuClick}
-          items={[
-            {
-              key: 'LoginScreen',
-              icon: <UserOutlined />,
-              label: 'Account',
-            },
-            {
-              key: 'BarcodeScanner',
-              icon: <ScanOutlined />,
-              label: 'Scan',
-            },
-            {
-              key: 'MoreInfo',
-              icon: <InfoCircleOutlined />,
-              label: 'More Info',
-            },
-          ]}
-        />
-      </Sider>
-      <Layout>
-        <Header
-          style={{
-            padding: 0,
-            background: colorBgContainer,
-          }}
+      <Layout
+      style={{
+        minHeight: '100vh',
+      }}
+      >
+        <Header style={{
+          position: 'sticky',
+          top: 0,
+          zIndex: 1,
+          width: '100%',
+          display: 'flex',
+          alignItems: 'center',
+        }}
         >
-          <Button
-            type="text"
-            icon={collapsed ? <MenuUnfoldOutlined /> : <MenuFoldOutlined />}
-            onClick={() => setCollapsed(!collapsed)}
-            style={{
-              fontSize: '16px',
-              width: 64,
-              height: 64,
-            }}
-          />
+          {/* Placeholder to keep the title centered */}
+          <div style={{ flex: 1 }}></div>
+          <div style={{ flex: 1, textAlign: 'center', color: 'white', fontSize: '20px' }}>KarmaCart</div>
+          {/* Hamburger Button */}
+          <div style={{ flex: 1, textAlign: 'right' }}>
+            <Button type="primary" onClick={showDrawer}> 
+              <MenuOutlined />
+            </Button>
+          </div>
         </Header>
+        <Drawer title="Menu" width="200px" placement="right" onClick={onClose} onClose={onClose} open={visible}>
+          <Menu defaultSelectedKeys={['1']} mode="inline" items={items} onClick={handleMenuClick} />
+        </Drawer>
         <Router>
             <Routes>
               <Route path="/" element={<LoginScreen />} />
@@ -89,7 +85,13 @@ const App = () => {
               <Route path="/more-info" element={<MoreInfo />} />
             </Routes>
         </Router>
-      </Layout>
+        <Footer
+          style={{
+            textAlign: 'center',
+          }}
+        >
+        KarmaCart Created by Anderson Buckles
+        </Footer>
     </Layout>
   );
 };

cb14edd9c1ca3a68a14965ad6bdc029590eaa84a
andersbuck <buckles.anderson@gmail.com>
2023-12-26 23:21:02 -0500

    Extend demo content

--- src/pages/MoreInfo.js
+++ src/pages/MoreInfo.js
@@ -18,7 +18,16 @@ const LoginScreen = () => {
       borderRadius: borderRadiusLG,
     }}
   >
-    Content
+    <p>long content</p>
+    {
+      // indicates very long content
+      Array.from({ length: 100 }, (_, index) => (
+        <React.Fragment key={index}>
+          {index % 20 === 0 && index ? 'more' : '...'}
+          <br />
+        </React.Fragment>
+      ))
+    }
   </Content>
   );
 };

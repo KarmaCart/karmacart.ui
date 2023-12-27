b8b98bb343b96806117345b552672c716f4a7368
andersbuck <buckles.anderson@gmail.com>
2023-12-26 23:21:59 -0500

    Structure page

--- src/pages/BarcodeScanner.js
+++ src/pages/BarcodeScanner.js
@@ -1,13 +1,43 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import Quagga from 'quagga';
+import { Layout, theme } from 'antd';
+
+const { Content } = Layout;
+
+let wasLoadingDialogHidden = false;
 
 const BarcodeScanner = () => {
+
+  const [isLoadingDialogVisible, setLoadingDialogVisible] = useState(false);
+
+  const showLoadingDialog = () => {
+    if(!wasLoadingDialogHidden) {
+      setLoadingDialogVisible(true);  
+    }
+  };
+
+  const hideLoadingDialog = () => {
+    setLoadingDialogVisible(false);
+    wasLoadingDialogHidden = true;
+  };
+
   useEffect(() => {
+
+    setTimeout(() => {
+      showLoadingDialog();
+    }, 400);
+
     Quagga.init({
+      locate: true,
       inputStream : {
         name : "Live",
         type : "LiveStream",
-        target: document.querySelector('#scanner-container')    // Or a reference to a DOM element
+        target: document.querySelector('#scanner-container'),
+        constraints: {
+          width: 300,
+          height: 300,
+          facingMode: "environment"
+        },
       },
       decoder : {
         readers : [
@@ -31,6 +61,7 @@ const BarcodeScanner = () => {
         }
         console.log("Initialization finished. Ready to start");
         Quagga.start();
+        hideLoadingDialog();
     });
 
     Quagga.onDetected((data) => {
@@ -44,7 +75,27 @@ const BarcodeScanner = () => {
     };
   }, []);
 
-  return <div id="scanner-container" />;
+  const {
+    token: { colorBgContainer, borderRadiusLG },
+  } = theme.useToken();
+
+  return (
+    <Content
+    style={{
+      margin: '24px 16px',
+      padding: 24,
+      minHeight: 280,
+      background: colorBgContainer,
+      borderRadius: borderRadiusLG,
+      alignItems: 'center',
+    }}
+    >
+    <div style={{fontSize: '20px'}}>Scan</div>
+    <div style={{textAlign: 'center', fontSize: '20px', display: isLoadingDialogVisible ? 'block' : 'none'}}>Loading...</div>
+    <div id="scanner-container">
+    </div>
+  </Content>
+  );
 };
 
 export default BarcodeScanner;

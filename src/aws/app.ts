d8dfdfb5fecf7d8f5314c4f09042449b0a8f9f4f
andersbuck <buckles.anderson@gmail.com>
2023-12-30 21:45:42 -0500

    Add github action to run aws cdk deployment

---
+++ src/aws/app.ts
@@ -0,0 +1,12 @@
+#!/usr/bin/env node
+import 'source-map-support/register';
+import { App } from 'aws-cdk-lib';
+import { KarmaCartUiStack } from './cdk-stack';
+
+const app = new App();
+new KarmaCartUiStack(app, 'KarmaCartUiStack', {
+  env: {
+    region: 'us-east-2',
+    account: '740207786562'
+  },
+});
---
+++ src/aws/cdk-stack.ts
@@ -0,0 +1,25 @@
+import { Stack, StackProps, RemovalPolicy }from 'aws-cdk-lib';
+import { Bucket } from 'aws-cdk-lib/aws-s3';
+import { Construct } from "constructs";
+import { BucketDeployment, Source } from 'aws-cdk-lib/aws-s3-deployment';
+
+export class KarmaCartUiStack extends Stack {
+  constructor(scope: Construct, id: string, props?: StackProps) {
+    super(scope, id, props);
+
+    // The code that defines your stack goes here
+    const myBucket = new Bucket(this, 'karma-cart-ui', {
+      publicReadAccess: true,
+      removalPolicy: RemovalPolicy.DESTROY,
+      websiteIndexDocument: 'index.html',
+    });
+    const deployment = new BucketDeployment(
+      this,
+      'deployStaticWebsite',
+      {
+        sources: [Source.asset('../build')],
+        destinationBucket: myBucket,
+      }
+    );
+  }
+}

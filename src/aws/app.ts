158929c6f2d8fb1545027308f64523c901fac010
andersbuck <buckles.anderson@gmail.com>
2024-01-05 15:25:47 -0500

    Remove environment logic since it will be determined in the deploy scripts

--- src/aws/app.ts
+++ src/aws/app.ts
@@ -6,20 +6,8 @@ import { KarmaCartUiStack } from './cdk-stack';
 let karamcartEnvironment = process.env.KARAMCART_ENVIRONMENT;
 let awsAccountNumber = process.env.AWS_ACCOUNT_NUMBER;
 
-// Validate that the environment was set with an expected value.
-if (!karamcartEnvironment) {
-  throw new Error("KaramCart Environment not set!");
-} else {
-  let isEngEnv = karamcartEnvironment === 'eng'
-  let isProdEnv = karamcartEnvironment === 'prod'
-  let isValidEnv = isEngEnv || isProdEnv;
-  if (!isValidEnv) {
-    throw new Error("KarmaCart Environment not valid!");
-  }
-}
-
 const app = new App();
-new KarmaCartUiStack(app, 'KarmaCartUiStack', karamcartEnvironment, {
+new KarmaCartUiStack(app, 'KarmaCartUiStack', karamcartEnvironment!, {
   env: {
     region: 'us-east-2',
     account: awsAccountNumber

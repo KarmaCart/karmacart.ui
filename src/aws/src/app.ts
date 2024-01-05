df1536e7eb162bbce6ae6ac2eaaa0e343986ab56
andersbuck <buckles.anderson@gmail.com>
2024-01-05 15:50:55 -0500

    Remove environment from bucket name

--- src/aws/src/app.ts
+++ src/aws/src/app.ts
@@ -3,11 +3,10 @@ import 'source-map-support/register';
 import { App } from 'aws-cdk-lib';
 import { KarmaCartUiStack } from './cdk-stack';
 
-const karamcartEnvironment = process.env.KARAMCART_ENVIRONMENT;
 const awsAccountNumber = process.env.AWS_ACCOUNT_NUMBER;
 
 const app = new App();
-new KarmaCartUiStack(app, 'KarmaCartUiStack', karamcartEnvironment!, {
+new KarmaCartUiStack(app, 'KarmaCartUiStack', {
   env: {
     region: 'us-east-2',
     account: awsAccountNumber
--- src/aws/src/cdk-stack.ts
+++ src/aws/src/cdk-stack.ts
@@ -4,11 +4,11 @@ import { Construct } from "constructs";
 import { BucketDeployment, Source } from 'aws-cdk-lib/aws-s3-deployment';
 
 export class KarmaCartUiStack extends Stack {
-  constructor(scope: Construct, id: string, environment: string, props?: StackProps) {
+  constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
     // The code that defines your stack goes here
-    const myBucket = new Bucket(this, `karma-cart-ui-${environment}`, {
+    const myBucket = new Bucket(this, `karma-cart-ui`, {
       blockPublicAccess: {
         blockPublicAcls: false,
         blockPublicPolicy: false,

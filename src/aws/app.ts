14418a365094c88b8654859e7625294be4d91ffc
andersbuck <buckles.anderson@gmail.com>
2024-01-05 22:30:56 -0500

    Add wildcard to domain

--- src/aws/app.ts
+++ src/aws/app.ts
@@ -13,5 +13,5 @@ new KarmaCartUiStack(app, 'KarmaCartUiStack', {
   },
   stackName: 'KarmaCartUiStack',
   sourcePath: 'build',
-  domainName: 'andersbuck.dev',
+  domainName: '*.andersbuck.dev',
 });

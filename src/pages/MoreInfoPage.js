c7f53e39d91892999044ad4c02699d14fc304f44
andersbuck <buckles.anderson@gmail.com>
2024-01-08 00:56:56 -0500

    Update MoreInfoPage

--- src/pages/MoreInfoPage.js
+++ src/pages/MoreInfoPage.js
@@ -1,5 +1,7 @@
 import React from 'react';
-import { Layout, theme } from 'antd';
+import { Layout, theme, Row, Col, Button } from 'antd';
+import { GithubOutlined, LinkedinOutlined } from '@ant-design/icons';
+import './MoreInfoPage.css'; 
 
 const { Content } = Layout;
 
@@ -7,6 +9,19 @@ const MoreInfoPage = () => {
   const {
     token: { colorBgContainer, borderRadiusLG },
   } = theme.useToken();
+
+  const socialLinks = [
+    {
+      type: 'GitHub',
+      icon: <GithubOutlined />,
+      url: 'https://github.com/andersbuck',
+    },
+    {
+      type: 'LinkedIn',
+      icon: <LinkedinOutlined />,
+      url: 'https://www.linkedin.com/in/andersbuck',
+    },
+  ];
 
   return(
   <Content
@@ -16,18 +31,22 @@ const MoreInfoPage = () => {
       minHeight: 280,
       background: colorBgContainer,
       borderRadius: borderRadiusLG,
+      textAlign: 'center'
     }}
   >
-    <p>long content</p>
-    {
-      // indicates very long content
-      Array.from({ length: 100 }, (_, index) => (
-        <React.Fragment key={index}>
-          {index % 20 === 0 && index ? 'more' : '...'}
-          <br />
-        </React.Fragment>
-      ))
-    }
+    <p>This demo application was created by Anderson Buckles</p>
+    <p>Find out more about this demo <a href='https://github.com/KarmaCart'>on GitHub</a></p>
+    <div className="social-section">
+      <Row justify="center">
+        {socialLinks.map(link => (
+          <Col key={link.type}>
+            <Button type="link" href={link.url} target="_blank" icon={link.icon}>
+              {link.type}
+            </Button>
+          </Col>
+        ))}
+      </Row>
+    </div>
   </Content>
   );
 };

ce34f29ecd2cd6babb15500fdb93e6b81e072442
andersbuck <buckles.anderson@gmail.com>
2024-02-04 21:59:33 -0500

    Modify app description

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -41,7 +41,7 @@ const HomePage = ({setSelectedMenuKey}) => {
     <img src='/karmacart-logo-title.png' alt="logo-title" width={250} height={250} />
     <div style={titleStyle}>KarmaCart</div>
     <h2 style={{margin: '0'}}>Scan & Discover:</h2>
-    <p style={{fontSize: '15px', margin: '0', maxWidth: '500px', marginLeft: 'auto', marginRight: 'auto'}}>
+    <p style={{fontSize: '16px', margin: '0', maxWidth: '400px', marginLeft: 'auto', marginRight: 'auto'}}>
       KarmaCart aims to be a comprehensive tool for consumers to navigate the ethical and sustainability of products, empowering them to make informed and impactful choices.
     </p>
     <Button type="primary" size="large" style={{margin: '10px'}} onClick={() => {navigate('/scan');}}>Scan First Item</Button>

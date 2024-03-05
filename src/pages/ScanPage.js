b0e6be961c50bc4ddc780037068a4af41d4d5afc
andersbuck <buckles.anderson@gmail.com>
2024-03-04 22:45:46 -0500

    Add what is this modal

--- src/pages/ScanPage.js
+++ src/pages/ScanPage.js
@@ -1,5 +1,5 @@
-import React, { useEffect } from 'react';
-import { Layout, theme } from 'antd';
+import React, { useEffect, useState } from 'react';
+import { Layout, theme, Button, Modal } from 'antd';
 import BarcodeScanner from '../components/BarcodeScanner';
 import { useNavigate } from 'react-router-dom';
 import { SCAN_PAGE } from '../App';
@@ -11,6 +11,31 @@ const ScanPage = ({setSelectedMenuKey}) => {
     token: { colorBgContainer, borderRadiusLG },
   } = theme.useToken();
   let navigate = useNavigate();
+
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const showModal = () => {
+    setIsModalOpen(true);
+  };
+
+  const handleOk = () => {
+    setIsModalOpen(false);
+  };
+
+  const handleCancel = () => {
+    setIsModalOpen(false);
+  };
+
+  // Function to determine the qrBox size
+  const getQrBoxSize = () => {
+    const width = window.innerWidth;
+    // Set the size of the qrBox based on the screen width
+    if (width < 600) {
+        return "310px";
+    } else {
+        return "500px"; 
+    }
+  };
 
   useEffect(() => {
     setSelectedMenuKey(SCAN_PAGE);
@@ -36,11 +61,31 @@ const ScanPage = ({setSelectedMenuKey}) => {
       alignItems: 'center',
     }}
     >
-    <div style={{ textAlign: 'center', fontSize: '20px'}}>Barcode Scanner</div>
+    <div style={{ display: 'flex', justifyContent: 'center' }}>
+      <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'space-between', width: getQrBoxSize() }}>
+        <div style={{fontSize: '20px'}}>Barcode Scanner</div>
+        <Button type="link" onClick={showModal}>What is this?</Button>
+      </div>
+    </div>
     <BarcodeScanner
                 onResult={handleScanSuccess}
                 onError={handleScanFailure}
             />
+    <Modal
+      title="About the Barcode Scanner"
+      open={isModalOpen}
+      onOk={handleOk}
+      onCancel={handleCancel}
+      footer={[
+        <Button key="back" onClick={handleCancel}>
+          Close
+        </Button>,
+      ]}
+    >
+      <p>The barcode scanner helps you quickly find information about products by scanning their barcodes. Simply point your device's camera at a barcode to get started.</p>
+      <p>This tool can provide details such as company information, product information, and ethical ratings, helping you make informed purchasing decisions.</p>
+      <p>For a list of all products visit the Products page from the menu.</p>
+    </Modal>
   </Content>
   );
 };
